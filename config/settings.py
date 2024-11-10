d91af5f9ffd18245e3cc7599a4924975876db2a6
Little-Wretch <nazarr914@gmail.com>
2024-11-10 20:20:08 +0200

    ci: setup CI

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ORIGIN = os.getenv("CORS_ALLOW_ORIGIN").split(" ")
